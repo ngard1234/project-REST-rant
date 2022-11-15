8f1201fac16b6a4f86dbf3311da2a86c022fbc40
ngard1234 <neilgardner@verizon.net>
2022-11-15 12:13:40 -0500

    Adding 404 page route.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+// Get the environment variables.
+require('dotenv').config()
+
+// Require needed node modules.
 const express = require('express')
 const app = express()
 
@@ -5,4 +10,11 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+//Add route for a 404 Page error
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+// Listen to a port number defined by a local environment variable.
+app.listen(process.env.PORT)

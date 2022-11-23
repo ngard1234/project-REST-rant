9cab4a7e26e763235d7d2564f1d450f58ea3c242
ngard1234 <neilgardner@verizon.net>
2022-11-23 09:55:50 -0500

    Delete functionallity implemented

--- views/default.jsx
+++ views/default.jsx
@@ -15,7 +15,7 @@ function Def (html) {
                             <a href="/">Home</a>
                         </li>
                         <li>
-                            <a href="/">Places</a> 
+                            <a href="/places">Places</a> 
                         </li>
                         <li>
                             <a href="/places/new">Add Place</a>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,28 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <p>{ data.place.name }</p>
+            <p>{ data.place.city }</p>
+            <p>{ data.place.state }</p>
+            <p>{ data.place.cuisines }</p>
+            <img src={data.place.pic} alt={data.place.name} />
+            <p>Rating: Currently Unrated</p>
+            <p>Comments : No comments as yet! </p>
+
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>     
+
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">
+                Delete
+            </button>
+            </form> 
+     
           </main>
         </Def>
     )
